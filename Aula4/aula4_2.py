a8a33dd858777569faf148c91f95cab22c2a28ad
ruanmarcoss <silva.ruanmarcos@gmail.com>
2019-11-13 10:17:30 -0300

    aula4_2

---
+++ Aula4/aula4_2.py
@@ -0,0 +1,28 @@
+# Aula 4_2 13-11-2019
+# Booleanas
+
+# validador = false
+# validador = true
+
+idade = 18
+validador = ( idade == 18 )
+print(validador)
+
+validador = ( idade != 18 )
+print(validador)
+validador = ( idade > 18 )
+print(validador)
+validador = ( idade < 18 )
+print(validador)
+validador = ( idade >= 18 )
+print(validador)
+validador = ( idade <= 18 )
+print(validador)
+
+
+validador = not True
+validador = not False
+
+sorteado = 'Marcela'
+validador = (sorteado== 'Mateus' and sorteado=='Marcela')
+validador = (sorteado== 'Mateus' or sorteado=='Marcela')
