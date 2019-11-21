86d82250ddb1f936dc2461fe6f6f8a4b8c77ebd7
unknown <900160@proway.treina>
2019-11-21 11:57:39 -0300

    Aula11

---
+++ 11-Aula11/aula11.py
@@ -0,0 +1,72 @@
+# Aula 11 21-11-2019
+# Prova
+
+
+#--- 1- Crie um programa que calcule o imposto ISS de um serviço de desenvolvimento de sofware
+#--- Utilizar métodos
+#--- 2- Calcule a rentabilidade anual de um investimento baseando-se em sua rentabilidade mensal(juros compostos) a rentabilidade deve ser apresentada em % e R$
+#--- Utilizar métodos
+#--- 3- Crie um programa para calculo de investimentos, solicitar quantas cotas deseja comprar(considerar valor do tesouro selic hoje) 
+#--- calcular o valor total ate o vencimento do titulo, utilizar metodos
+#--- 4-
+#--- 5-
+
+###############################################################################################
+
+#Exercício 1 - RESOLUÇÃO
+# print('='*50, '\n')
+# from calculo import calculo_iss
+
+# valor_servico = float(input('digite o valor_servico: '))
+# aliquota_iss = float(input('digite a aliquota_iss: '))
+
+# iss_recolhido = valor_servico * aliquota_iss
+
+# print(f'valor_servico: {valor_servico}')
+# print(f'aliquota_iss: {aliquota_iss}')
+# print(f'iss_recolhido: {valor_servico} * {aliquota_iss} : {valor_servico * aliquota_iss}')
+# print('='*50, '\n')
+
+###############################################################################################
+
+#Exercício 2 - RESOLUÇÃO
+
+# ('='*10, '\n'*3)
+# from calculo import valor_final
+
+# valor_inicial = float( input('valor inicial: '))
+# taxa = float(input('valor da taxa: '))
+# valor_final = valor_final(valor_inicial, taxa)
+
+# print(f'valor_inicial: {valor_inicial} * meses: {taxa} = {valor_final:.2f}' )
+# ('='*10, '\n'*3)
+
+###############################################################################################
+
+
+#Exercício 3 - RESOLUÇÃO
+
+# 01/03/2020
+# 0,01
+# 10410.00
+# selic +0.02 ao ano
+
+print('='*10, '\n')
+
+
+investimento = float(input('digite o valor do investimento: '))
+cotas = float(input('digite a quantidade das cotas: '))
+rendimento = float (input('digite o valor do rendimento: '))
+
+
+
+
+
+
+
+
+
+
+
+
+print('='*10, '\n')
---
+++ 11-Aula11/calculo.py
@@ -0,0 +1,19 @@
+# def calculo_iss(aliquota_iss, valor_servico):
+#     resultado = valor_servico % aliquota_iss
+#     return iss_recolhido
+
+
+# def valor_final(valor_inicial, taxa):
+#     resultado = valor_inicial * (1+taxa/100)**12
+#     return resultado
+
+
+
+def cota(quantidades_cotas, valor_investido):
+    if(cota<1):
+        taxa_selic = valor_investido * quantidades_cotas
+    elif(cota<2):
+        taxa_selic = valor_investido * quantidades_cotas
+    
+        
+    
