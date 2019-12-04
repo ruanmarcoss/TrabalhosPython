86996bcdae923b9d012769ee857f54a04af58d7e
RuanSilva <silva.ruanmarcos@gmail.com>
2019-12-03 23:49:48 -0200

    Exercicio4 - Aula18

--- 18-Aula18/exercicio4.py
+++ 18-Aula18/exercicio4.py
@@ -20,17 +20,30 @@
 # cervejas.
 
 cerveja = (('marca', 'tipo', 'ibu','preço'),
-            ('Skol','IPA','ultra-leve',3.50),
-           ('Brahma','lager','leve/media',3.45),
-          ('Kaiser','Americam Larger','leve',2.35),
-            ('Sol','larger mão','agua',1.19)
-          )
-
+                ('Skol','IPA','ultra-leve',3.50),
+              ('Brahma','lager','leve/media',3.45),
+              ('Kaiser','Americam Larger','leve',2.35),
+                ('Sol','larger mão','agua',1.19)
+              )
 cabecalho = cerveja[0]
 dados = cerveja[1:]
+
 
 for dados_cerveja in dados:
     print(f'{cabecalho[0]} {dados_cerveja[0]}')
     print(f'{cabecalho[1]} {dados_cerveja[1]}')
     print(f'{cabecalho[2]} {dados_cerveja[2]}')
     print(f'{cabecalho[3]} {dados_cerveja[3]}')
+
+
+def dados_cerveja():
+    lista = []
+    for j in range():  
+        dicionario = {}
+        for i in cerveja:
+            dicionario[i] = (f'{i}')
+        lista.append(dicionario)
+    return lista
+
+dados_cerveja()
+
