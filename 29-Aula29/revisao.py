44f76cf5f8d21de89ac3770ae2a1628dbb5a1566
unknown <900160@proway.treina>
2020-01-06 11:46:41 -0300

    Revisão, ex HBSIS AirLInes

---
+++ 29-Aula29/revisao.py
@@ -0,0 +1,14 @@
+#----- Listas
+pessoasL = ['Gustavo','Maykao','Josue Rei das Listas']
+#----- Tuplas
+pessoasT = ('Gustavo','Maykao','Josue Rei das Listas')
+#----- Dicionários
+pessoasD = {'aluno':'Gustavo', 'professor':'Maykao', 'rei':'Josue Rei das Listas'}
+
+print(pessoasD)
+print(pessoasT[1])
+print(pessoasL[2])
+print(pessoasD['aluno'])
+
+for i in pessoasL:
+    print(i)
