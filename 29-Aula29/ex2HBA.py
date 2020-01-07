d7bb3b18b6f52802f2d4082689d21076fff6b76c
unknown <900160@proway.treina>
2020-01-07 10:40:27 -0300

    .

--- 29-Aula29/ex2HBA.py
+++ 29-Aula29/ex2HBA.py
@@ -10,14 +10,11 @@ carro = []
 aviao = []
 terminal = [el1,el2,el3,el4,el5,el6,el7,el8]
 numero_viagem=0
-    
 def viagem_fortwo():
     global numero_viagem
     numero_viagem += 1
     print(f'\n========== Viagem {numero_viagem} ==========')
     print(f'Estão no terminal: {terminal}')
-    
-
 def embarque(mot, pas):
     terminal.remove(mot)
     terminal.remove(pas)    
@@ -29,7 +26,6 @@ def embarque(mot, pas):
     aviao.append(pas)
     print(f'O {mot} volta no Fortwo para o terminal')
     terminal.append(mot)   
-
 def embarques(mot,pas):
     terminal.remove(mot)
     terminal.remove(pas)
@@ -41,38 +37,31 @@ def embarques(mot,pas):
     aviao.append(mot)
     aviao.append(pas)
     print(f'O {mot} e o {pas} saem do fortwo e entram no avião')
-    
 for viagem in range(1,8):
     if viagem == 1:
         viagem_fortwo()
         embarque(el4, el5)
         print(f'Estão no avião: {aviao}')
-
     elif viagem == 2:
         viagem_fortwo()
         embarque(el4,el6)
         print(f'Estão no avião: {aviao}')
-
     elif viagem == 3:
         viagem_fortwo()
         embarque(el1,el2)
         print(f'Estão no avião: {aviao}')
-
     elif viagem == 4:
         viagem_fortwo()
         embarque(el1,el3)
         print(f'Estão no avião: {aviao}')
-
     elif viagem == 5:
         viagem_fortwo()
         embarque(el1,el4)
         print(f'Estão no avião: {aviao}')
-
     elif viagem == 6:
         viagem_fortwo()
         embarque(el8,el1)
         print(f'Estão no avião: {aviao}')
-
     elif viagem == 7:
         viagem_fortwo()
         embarques(el7,el8)
