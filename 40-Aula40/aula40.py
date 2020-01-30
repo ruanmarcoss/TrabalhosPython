967af92ff4f51733cb3c2dadccc63aa7b7c3e508
unknown <900160@proway.treina>
2020-01-30 08:11:53 -0300

    Exercícios // Ambiente-Virtual

---
+++ 40-Aula40/aula40.py
@@ -0,0 +1,16 @@
+#Flask RestFul
+#GET, POST, PUT, DELETE/ São métodos http.
+from flask import Flask
+from flask_restful import Api
+from Controllers.cerveja_controller import CervejaController
+
+app = Flask(__name__)
+api = Api(app)
+
+api.add_resource(CervejaController, '/api/cerveja')
+
+@app.route('/')
+def inicio():
+    return'Bem vindo a API'
+
+app.run(debug=True, port=80)
